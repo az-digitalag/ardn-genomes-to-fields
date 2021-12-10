675b5022fd259ee401e56263184cb38616d1a60f
Kristina Riemer <kristina.riemer@weecology.org>
2021-12-10 09:42:28 -0700

    Update observations script to get sample of G2F data

--- V2/download_observations.R
+++ V2/download_observations.R
@@ -1,22 +1,35 @@
-https://imagebreed.org/brapi/v2/observationunits?studyDbId=241&access_token=
-https://imagebreed.org/brapi/v2/observations?studyDbId=241&access_token=
-
 ###### Read in libraries ######
 library(jsonlite)
 
 ###### Specify study IDs ######
 studies <- c(241)
+vars <- c(92193) #Grain Yield [bu/acre]
 
 ###### Download, reformat, and save JSON file for each study ######
 key <- readLines("V2/api_key.txt")
 base_url <- "https://imagebreed.org"
-observation_base_url <- paste0(base_url, "/brapi/v2/observations")
+obs_base_url <- paste0(base_url, "/brapi/v2/observations")
 
 for(study in studies){
-  observation_url <- paste0(observation_base_url, "?studyDbId=", study, "&pageSize=2", "&access_token=", key)
-  #observation_list <- fromJSON(observation_url)
-  # observation_json <- list(studyDbId = as.character(study), data = observation_list)
-  # observation_json_final <- list(studies = list(observation_json))
-  # write(toJSON(observation_json_final, pretty = TRUE, auto_unbox = TRUE),
-  #       file = paste0("V2/observation_", study, ".json"))
+  observations_json <- list()
+  for(var in vars){
+    pagesize_url <- paste0(obs_base_url, "?studyDbId=", study,
+                           "&observationVariableDbId=", var, "&pageSize=2", 
+                           "&access_token=", key)
+    pagesize_download <- fromJSON(pagesize_url)
+    pagesize_count <- 30 #pagesize_download$metadata$pagination$totalCount
+    if (pagesize_count != 0) {
+      observation_url <- paste0(obs_base_url, "?studyDbId=", study,
+                                "&observationVariableDbId=", var, "&pageSize=", 
+                                pagesize_count, "&access_token=", key)
+      observation_list <- fromJSON(observation_url)
+      observation_json <- list(studyDbId = as.character(study),
+                               observationVariableDbId = as.character(var),
+                               data = observation_list)
+    }
+    observations_json <- append(observations_json, list(observation_json))
+    observations_json_final <- list(observed = observations_json)
+  }
+  write(toJSON(observations_json_final, pretty = TRUE, auto_unbox = TRUE),
+        file = paste0("V2/observations_", study, ".json"))
 }
